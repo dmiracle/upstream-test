755139a428a61164cb6871db699a323df33b42df
Dylan Miracle <dylan.miracle@gmail.com>
2020-10-25 13:38:18 -0500

    more git methods and tests

--- githubutil.py
+++ githubutil.py
@@ -1,6 +1,7 @@
 import requests
 from decouple import config
 import json
+import subprocess
 import os
 
 class GithubUtil:
@@ -23,6 +24,47 @@ class GithubUtil:
         }
         r = requests.delete(f"https://api.github.com/repos/{user}/{name}", headers=headers)
         return(r)
-    def cloneRepo(self, url):
-        localDirName = "temp"
-        gitCommand = f"git clone {url}"
+    
+    def cloneRepo(self, url, localDirName):
+        gitCommand = ["git", "clone", url, localDirName]
+        ec = subprocess.run(gitCommand)
+        repopath = os.path.abspath(localDirName)
+        return ec, repopath
+
+    def getRemote(self, project_path):
+        os.chdir(project_path)
+        show_remote = ["git", "remote", "-v"]
+        out = subprocess.run(show_remote, capture_output=True)
+        return out
+
+    def rmRemote(self, project_path):
+        os.chdir(project_path)
+        rm_remote = ["git", "remote", "rm", "origin"]
+        out = subprocess.run(rm_remote, capture_output=True)
+        return out
+
+    def addRemote(self, project_path, new_remote):
+        os.chdir(project_path)
+        add_remote = ["git", "remote", "add", "origin", "new_remote"]
+        out = subprocess.run(add_remote, capture_output=True)
+        return out
+
+    def firstPush(self, project_path, branch="main"):
+        os.chdir(project_path)
+        set_main = ["git", "branch", "-M", branch]
+        push_main = ["git", "push", "-u", "origin", branch]
+        out1 = subprocess.run(set_main)
+        out2 = subprocess.run(push_main)
+        return out1, out2
+    
+    def changeRemote(self, project_path, new_remote):
+        out = []
+        os.chdir(project_path)
+        out.append(self.rmRemote(project_path))
+        out.append(self.createRepo(new_remote))
+        out.append(self.addRemote(project_path, new_remote))
+        out.append(self.firstPush(project_path))
+        return out
+
+
+        return out
--- projectrTest.py
+++ projectrTest.py
@@ -1,13 +1,41 @@
+import unittest
 import githubutil as gh
 import yamlutil as yu
 
-yu_agent = yu.YamlUtil()
-yu_agent.load("config.yml")
-gh_agent = gh.GithubUtil()
-r = gh_agent.createRepo("testymctestface")
-print(r.status_code)
-print(r.text)
+class TestProject(unittest.TestCase):
 
-r = gh_agent.deleteRepo("dmiracle", "testymctestface")
-print(r.status_code)
-print(r.text)
+    yu_agent = yu.YamlUtil()
+    gh_agent = gh.GithubUtil()
+    
+    @unittest.skip
+    def test_load_config(self):
+        self.yu_agent.load("config.yml")
+
+    @unittest.skip
+    def test_create_repo(self):
+        r = self.gh_agent.createRepo("testymctestface")
+        print(r.status_code)
+        print(r.text)
+
+    @unittest.skip("This is too much power")
+    def test_delete_repo(self):
+        r = self.gh_agent.deleteRepo("dmiracle", "testymctestface")
+        print(r.status_code)
+        print(r.text)
+
+    @unittest.skip
+    def test_clone_repo(self):
+        ec, path = self.gh_agent.cloneRepo('https://github.com/dmiracle/jupyter-starter.git', 'temp')
+        print(ec)
+        print(path)
+
+    # @unittest.skip
+    def test_change_remote(self):
+        ec, path = self.gh_agent.cloneRepo('https://github.com/dmiracle/jupyter-starter.git', 'temp')
+        print(ec)
+        print(path)
+        out = self.gh_agent.changeRemote(path, 'test2')
+        print(out)
+
+if __name__ == '__main__':
+    unittest.main()
